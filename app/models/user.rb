f855c0ca62b22b1bfba23176760985df6cea3154
sakamot <matuhana.8@gmail.com>
2018-03-04 17:56:55 +0900

    add devise user

---
+++ app/models/user.rb
@@ -0,0 +1,20 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable,
+         authentication_keys: [:username]
+
+  validates_uniqueness_of :username
+  validates_presence_of :username
+
+  def self.find_first_by_auth_conditions(warden_conditions)
+    conditions = warden_conditions.dup
+    if login = conditions.delete(:login)
+      #認証の条件式を変更する
+      where(conditions).where(["username = :value", { :value => username }]).first
+    else
+      where(conditions).first
+    end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
   root to: 'articles#index'
   resources :articles
+
+  devise_for :users, controllers: {
+    sessions: 'users/sessions'
+  }
 end
